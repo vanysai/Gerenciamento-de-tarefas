3973492eb4bc0538c98461b593fd5e243d08cf26
Jaíza Freire <99412036+vanysai@users.noreply.github.com>
2022-06-05 17:11:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
-let tarefas= []
+let tarefas = []
 
 const addTarefa = () => {
-    let tarefa = document.getElementById("tarefa").value
-
+    let tarefa = document.getElementById('tarefa').value
     document.getElementById('tarefa').value = ""
+
     if(localStorage.getItem('tarefas') != null){
         tarefas = JSON.parse(localStorage.getItem('tarefas'))
-
     }
     let data = new Date();
-    let date = `${date.getUTCDay()}/${data.getUTCMonth()}/${data.getUTCFullYear()}`;
+    let date = `${data.getUTCDay()}/${data.getUTCMonth()}/${data.getUTCFullYear()}`
 
-tarefas.push([date, tarefa])
-localStorage.setItem("tarefas", JSON.stringify(tarefas))
+    tarefas.push([date, tarefa])
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
 
 }
+
 const list = () => {
-    let tbody = document.querySelector("#tr-tarefa")
-    if(localStorage.getItem("tarefas") != null){
-        tarefas = JSON.parse(localStorage.getItem(tarefas))
+    let tbody = document.querySelector('#tr-tarefa')
+
+    if (localStorage.getItem('tarefas') != null){
+        tarefas = JSON.parse(localStorage.getItem('tarefas'))
     } else {
-        tarefas.push(["Sem dados ", "Sem dados"])
+        tarefas.push(["Sem dados", "Sem dados"])
     }
+
     tarefas.forEach(element => {
-        tbody.innerHTML += "<tr><td>"+ element[0]+"</td><td>" + element[1]+"</td></tr>"
+        tbody.innerHTML +="<tr> <td>"+element[0]+"</td> <td>"+element[1]+"</td> </tr>" 
     });
+
     
 }
